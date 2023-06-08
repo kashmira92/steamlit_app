decf8586df99202314d863a701039fa2d6841dce
kashmira92 <48323327+kashmira92@users.noreply.github.com>
2023-06-07 22:24:22 -0400

    Update strealit_app.py
    added menu

--- strealit_app.py
+++ strealit_app.py
@@ -1,3 +1,10 @@
 # created the main python file
 import streamlit
 streamlit.title('Snowflake Streamlit Application')
+streamlit.header(' 🥣Breakfast Menu')
+streamlit.text('  🥗 Omega 3 & Blueberry Oatmeal')
+streamlit.text('  🥑Kale, Spinach & Rocket Smoothie')
+streamlit.text(' 🐔Hard-Boiled Free-Range Egg')
+streamlit.text(' 🥑🍞 Hard-Boiled Free-Range Egg')
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
