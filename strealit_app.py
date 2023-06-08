7be5a60696fe5491e8bfff19f392ebd9bcc588a7
kashmira92 <48323327+kashmira92@users.noreply.github.com>
2023-06-08 14:02:30 -0400

    Update strealit_app.py

--- strealit_app.py
+++ strealit_app.py
@@ -13,5 +13,5 @@ streamlit.dataframe(my_fruit_list)
 my_fruit_list= my_fruit_list.set_index('Fruit')
 streamlit.multiselect("Pick some fruits :", list(my_fruit_list.index))
 streamlit.dataframe(my_fruit_list)
-
+streamlit.multiselect("Pick some fruits :", list(my_fruit_list.index)),['Avacado', 'Strawberries'])
 
