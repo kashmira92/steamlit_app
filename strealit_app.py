a2bd27ba2b9e2c1b2652e991cdf4d995cab011a2
kashmira92 <48323327+kashmira92@users.noreply.github.com>
2023-06-14 08:33:38 -0400

    Update strealit_app.py

--- strealit_app.py
+++ strealit_app.py
@@ -35,5 +35,6 @@ my_cur.execute("SELECT * from fruit_load_list")
 my_data_rows = my_cur.fetchall()
 streamlit.header("The fruit load list contains:")
 streamlit.dataframe(my_data_rows)
-
+fruit_choice_j = streamlit.text_input('What fruit would you like information about?','jackfruit')
+streamlit.write('The user entered ', fruit_choice_j)
 
