1e52e227a32379fb6a14d6ee82449563ba45a1b1
kashmira92 <48323327+kashmira92@users.noreply.github.com>
2023-06-14 09:00:27 -0400

    Update strealit_app.py

--- strealit_app.py
+++ strealit_app.py
@@ -21,16 +21,26 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 # import requests
 streamlit.header("Fruityvice Fruit Advice!")
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
+try:
+  fruit_choice = streamlit.text_input('What fruit would you like information about?')
+  if not fruit_choice:
+    streamlit.error("Please select a frruit to get information.")
+   else:
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+    fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
+    streamlit.dataframe(fruityvice_normalized)
+    
+    
+# streamlit.write('The user entered ', fruit_choice)
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-# streamlit.text(fruityvice_response)
-# streamlit.text(fruityvice_response.json())
-fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
-# tablular form
-streamlit.dataframe(fruityvice_normalized)
-streamlit.stop()
+# fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+# # streamlit.text(fruityvice_response)
+# # streamlit.text(fruityvice_response.json())
+# fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
+# # tablular form
+# streamlit.dataframe(fruityvice_normalized)
+except URLError as e:
+  streamlit.stop()
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 # my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
