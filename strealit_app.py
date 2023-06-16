0c85a93db4eb21e04b1466817933a1b974915ef8
kashmira92 <48323327+kashmira92@users.noreply.github.com>
2023-06-16 10:10:58 -0400

    Update strealit_app.py

--- strealit_app.py
+++ strealit_app.py
@@ -58,8 +58,8 @@ except URLError as e:
 # # tablular form
 # streamlit.dataframe(fruityvice_normalized)
 
-# my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-# my_cur = my_cnx.cursor()
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cur = my_cnx.cursor()
 # # my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
 # my_cur.execute("SELECT * from fruit_load_list")
 # my_data_rows = my_cur.fetchall()
